419a2acc9f4e6a39ae23ee1fe71d52477ede971d
Tim Lencioni <tim.lencioni@gmail.com>
2024-09-15 17:10:29 -0600

    Implement add and get Piece methods

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -9,6 +9,10 @@ package chess;
 public class ChessBoard {
 
     public ChessBoard() {
+        private ChessPiece[][] board = new ChessPiece[8][8];
+        public ChessBoard() {
+
+        }
         
     }
 
@@ -19,7 +23,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +34,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow()][position.getColumn()];
     }
 
     /**
