75776eda3741db24f6662d047bdb244bae5ecb82
Tim Lencioni <tim.lencioni@gmail.com>
2024-09-17 14:19:51 -0600

    implement fully ChessMove

--- chess/shared/src/main/java/chess/ChessMove.java
+++ chess/shared/src/main/java/chess/ChessMove.java
@@ -8,22 +8,30 @@ package chess;
  */
 public class ChessMove {
 
+    private ChessPosition start;
+    private ChessPosition end;
+    private ChessPiece.PieceType promotionPiece;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+
+        this.start = startPosition;
+        this.end = endPosition;
+        this.promotionPiece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return this.start;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return this.end;
     }
 
     /**
@@ -33,6 +41,30 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return this.promotionPiece;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove move = (ChessMove) o;
+        return (start.equals(move.start) && end.equals(move.end)
+                && promotionPiece == move.promotionPiece);
+
+    }
+
+    @Override
+    public int hashCode() {
+        var promotionCode = (promotionPiece == null ?
+                9 : promotionPiece.ordinal());
+        return (71 * start.hashCode()) + end.hashCode() + promotionCode;
+    }
+
+    @Override
+    public String toString() {
+        var p = (promotionPiece == null ? "" : ":" + promotionPiece);
+        return String.format("%s:%s%s", start.toString(), end.toString(), p);
+    }
+
 }
