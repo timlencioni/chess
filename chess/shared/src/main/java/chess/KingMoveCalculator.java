e8ff06a165ad21555802cff07780d83a7df77137
Tim Lencioni <tim.lencioni@gmail.com>
2024-09-17 14:36:29 -0600

    add files for pieceMoves Calculator

---
+++ chess/shared/src/main/java/chess/KingMoveCalculator.java
@@ -0,0 +1,11 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class KingMoveCalculator implements MovementRule {
+    public Collection<ChessMove> moves(ChessBoard board, ChessPosition myPosition) {
+        //calculate king moves based on the position on the board
+        return new ArrayList<>(); //Return the datastructure with the king moves.
+    }
+}
--- chess/shared/src/main/java/chess/MovementRule.java
+++ chess/shared/src/main/java/chess/MovementRule.java
@@ -1,2 +1,118 @@
-package chess;public interface MovementRule {
+package chess;
+
+import java.util.Collection;
+
+public interface MovementRule {
+    public Collection<ChessMove> moves(ChessBoard board, ChessPosition myPosition);
 }
+
+
+/*
+public abstract class BaseMovement implements MovementRule {
+
+    @Override
+    protected void Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition, int row, int col,
+                                                        Collection<ChessMove> moves, boolean allowDistance) {
+        // TODO:: Calculate most piece rules.
+        // First step, look at proposed direction to move to.
+        // Check if their are pieces in the way
+        // Add new position to list of potential moves.
+    }
+
+    public abstract Collection<ChessMove> moves(ChessBoard board, ChessPosition myPosition);
+}
+
+public class BishopMovement extends BaseMovement {
+    @Override
+    public Collection<ChessMove> moves(ChessBoard board, ChessPosition myPosition) {
+        var moves = new HashSet<ChessMove>();
+        calculateMoves(board, myPosition, -1, -1, moves, true);
+        calculateMoves(board, myPosition, -1,  1, moves, true);
+        calculateMoves(board, myPosition,  1, -1, moves, true);
+        calculateMoves(board, myPosition,  1,  1, moves, true);
+        return moves;
+    }
+}
+
+public class KingMovement extends BaseMovement {
+    @Override
+    public Collection<ChessMove> moves(ChessBoard board, ChessPosition myPosition) {
+        var moves = new HashSet<ChessMove>();
+        calculateMoves(board, myPosition,  0,  1, moves, false);
+        calculateMoves(board, myPosition,  0, -1, moves, false);
+        calculateMoves(board, myPosition,  1,  0, moves, false);
+        calculateMoves(board, myPosition, -1,  0, moves, false);
+        calculateMoves(board, myPosition, -1, -1, moves, false);
+        calculateMoves(board, myPosition, -1,  1, moves, false);
+        calculateMoves(board, myPosition,  1, -1, moves, false);
+        calculateMoves(board, myPosition,  1,  1, moves, false);
+        return moves;
+    }
+}
+
+public class QueenMovement extends BaseMovement {
+    @Override
+    public Collection<ChessMove> moves(ChessBoard board, ChessPosition myPosition) {
+        var moves = new HashSet<ChessMove>();
+        calculateMoves(board, myPosition,  0,  1, moves, true);
+        calculateMoves(board, myPosition,  0, -1, moves, true);
+        calculateMoves(board, myPosition,  1,  0, moves, true);
+        calculateMoves(board, myPosition, -1,  0, moves, true);
+        calculateMoves(board, myPosition, -1, -1, moves, true);
+        calculateMoves(board, myPosition, -1,  1, moves, true);
+        calculateMoves(board, myPosition,  1, -1, moves, true);
+        calculateMoves(board, myPosition,  1,  1, moves, true);
+        return moves;
+    }
+}
+
+public class RookMovement extends BaseMovement {
+    @Override
+    public Collection<ChessMove> moves(ChessBoard board, ChessPosition myPosition) {
+        var moves = new HashSet<ChessMove>();
+        calculateMoves(board, myPosition,  0,  1, moves, true);
+        calculateMoves(board, myPosition,  0, -1, moves, true);
+        calculateMoves(board, myPosition,  1,  0, moves, true);
+        calculateMoves(board, myPosition, -1,  0, moves, true);
+        return moves;
+    }
+}
+
+public class PawnMovement extends BaseMovement {
+    @Override
+    public Collection<ChessMove> moves(ChessBoard board, ChessPosition myPosition) {
+        var moves = new HashSet<ChessMove>();
+        calculateMoves(board, myPosition,  1,  0, moves, false);
+        calculateMoves(board, myPosition,  1, -1, moves, false);
+        calculateMoves(board, myPosition,  1,  1, moves, false);
+        return moves;
+    }
+}
+
+
+/*
+{
+
+        Collection<ChessMove> possibleMoves = new HashSet<ChessMove>;
+        int currRow = myPosition.getRow();
+        int currCol = myPosition.getColumn();
+
+        // Return a collection of possible moves
+
+        if (ChessBoard.getPiece(currRow + 1, currCol) == null) {
+            possibleMoves.add(ChessMove(myPosition, ChessPosition(currRow + 1, currCol)));
+        }
+        if (currRow == 2 && ChessBoard.getPiece(currRow + 1, currCol) == null &&
+                ChessBoard.getPiece(currRow + 2, currCol) == null) {
+            possibleMoves.add(ChessMove(myPosition, ChessPosition(currRow + 2, currCol)));
+        }
+        if (ChessBoard.getPiece(currRow + 1, currCol + 1) != null) {
+            possibleMoves.add(ChessMove(myPosition, ChessPosition(currRow + 1, currCol + 1)));
+        }
+        if (ChessBoard.getPiece(currRow + 1, currCol - 1) != null) {
+            possibleMoves.add(ChessMove(myPosition, ChessPosition(currRow + 1, currCol)));
+        }
+
+        return possibleMoves;
+    }
+ */
