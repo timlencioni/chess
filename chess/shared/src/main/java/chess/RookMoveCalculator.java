4ad7396c081966143930dbdfc7611db67a13cc8d
Tim Lencioni <tim.lencioni@gmail.com>
2024-09-18 17:27:44 -0600

    Rook movements done

--- chess/shared/src/main/java/chess/RookMoveCalculator.java
+++ chess/shared/src/main/java/chess/RookMoveCalculator.java
@@ -13,37 +13,72 @@ public class RookMoveCalculator implements MovementCalculator {
 
         // for loops which runs through all diagonals in a given direction until it ends
         //FIXME: Can we create a function that does the if else statement separately?
-        //FIXME: Also fix the Position in getPiece method call
         for (int i = 1; i < 8; i++) { // Up
             int rowToAdd = row + i;
-            if (board.getPiece(myPosition) != null && rowToAdd > 0 && rowToAdd < 9) {
-                possibleMoves.add(new ChessMove(myPosition, new ChessPosition(rowToAdd, col), null));
+            ChessPosition newPos = new ChessPosition(rowToAdd, col);
+            if (rowToAdd > 0 && rowToAdd < 9) {
+                if (board.getPiece(newPos) != null
+                        && board.getPiece(newPos).getTeamColor() == board.getPiece(myPosition).getTeamColor()) {
+                    break;
+                }
+                else if (board.getPiece(newPos) != null
+                        && board.getPiece(newPos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    possibleMoves.add(new ChessMove(myPosition, new ChessPosition(rowToAdd, col), null));
+                    break;
+                }
+                else possibleMoves.add(new ChessMove(myPosition, new ChessPosition(rowToAdd, col), null));
             }
-            else break;
         }
 
         for (int i = 1; i < 8; i++) { // Right
             int colToAdd = col + i;
-            if (board.getPiece(myPosition) != null && colToAdd > 0 && colToAdd < 9) {
-                possibleMoves.add(new ChessMove(myPosition, new ChessPosition(row, colToAdd), null));
+            ChessPosition newPos = new ChessPosition(row, colToAdd);
+            if (colToAdd > 0 && colToAdd < 9) {
+                if (board.getPiece(newPos) != null
+                        && board.getPiece(newPos).getTeamColor() == board.getPiece(myPosition).getTeamColor()) {
+                    break;
+                }
+                else if (board.getPiece(newPos) != null
+                        && board.getPiece(newPos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    possibleMoves.add(new ChessMove(myPosition, new ChessPosition(row, colToAdd), null));
+                    break;
+                }
+                else possibleMoves.add(new ChessMove(myPosition, new ChessPosition(row, colToAdd), null));
             }
-            else break;
         }
 
         for (int i = 1; i < 8; i++) { // Left
             int colToAdd = col - i;
-            if (board.getPiece(myPosition) != null && colToAdd > 0 && colToAdd < 9) {
-                possibleMoves.add(new ChessMove(myPosition, new ChessPosition(row, colToAdd), null));
+            ChessPosition newPos = new ChessPosition(row, colToAdd);
+            if (colToAdd > 0 && colToAdd < 9) {
+                if (board.getPiece(newPos) != null
+                        && board.getPiece(newPos).getTeamColor() == board.getPiece(myPosition).getTeamColor()) {
+                    break;
+                }
+                else if (board.getPiece(newPos) != null
+                        && board.getPiece(newPos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    possibleMoves.add(new ChessMove(myPosition, new ChessPosition(row, colToAdd), null));
+                    break;
+                }
+                else possibleMoves.add(new ChessMove(myPosition, new ChessPosition(row, colToAdd), null));
             }
-            else break;
         }
 
         for (int i = 1; i < 8; i++) { // Down
             int rowToAdd = row - i;
-            if (board.getPiece(myPosition) != null && rowToAdd > 0 && rowToAdd < 9) {
-                possibleMoves.add(new ChessMove(myPosition, new ChessPosition(rowToAdd, col), null));
+            ChessPosition newPos = new ChessPosition(rowToAdd, col);
+            if (rowToAdd > 0 && rowToAdd < 9) {
+                if (board.getPiece(newPos) != null
+                        && board.getPiece(newPos).getTeamColor() == board.getPiece(myPosition).getTeamColor()) {
+                    break;
+                }
+                else if (board.getPiece(newPos) != null
+                        && board.getPiece(newPos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    possibleMoves.add(new ChessMove(myPosition, new ChessPosition(rowToAdd, col), null));
+                    break;
+                }
+                else possibleMoves.add(new ChessMove(myPosition, new ChessPosition(rowToAdd, col), null));
             }
-            else break;
         }
 
         return possibleMoves;
