2af803382fa8e1927351a3cbc8ee63cccf17b880
Tim Lencioni <tim.lencioni@gmail.com>
2024-09-18 18:02:53 -0600

    Pawn movement done

--- chess/shared/src/main/java/chess/BlackPawnMoveCalculator.java
+++ chess/shared/src/main/java/chess/BlackPawnMoveCalculator.java
@@ -15,35 +15,40 @@ public class BlackPawnMoveCalculator implements MovementCalculator {
         int col = myPosition.getColumn();
         int rowToAdd = row - 1; // Can only go forward one row
 
-        if (row < 8) { // If the pawn is on the end, we can't do anything, and it should be promoted anyway
-            for (int i = -1; i < 2; i++) {
-                int colToAdd = col + i; //Change column to go forward left, straight, and right.
-                ChessPosition newPos = new ChessPosition(rowToAdd, colToAdd);
-                if (col != colToAdd) {
-                    if (0 < colToAdd && colToAdd < 8) {
-                        if (board.getPiece(newPos) != null) {
-                            if (board.getPiece(newPos).getTeamColor() != null
-                                    && board.getPiece(newPos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                                possibleMoves.add(new ChessMove(myPosition, newPos, promo));
-                            }
-                        }
-                    }
-                } else {
-                    if (board.getPiece(newPos) == null) {
-                        possibleMoves.add(new ChessMove(myPosition, newPos, promo));
-                    }
+
+        for (int i = -1; i < 2; i++) {
+            int colToAdd = col + i; //Change column to go forward left, straight, and right.
+            ChessPosition newPos = new ChessPosition(rowToAdd, colToAdd);
+            if (col != colToAdd && 0 < colToAdd && colToAdd < 8 && board.getPiece(newPos) != null) {
+                if (board.getPiece(newPos).getTeamColor() != null
+                        && board.getPiece(newPos).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    if (row == 2) {
+                        addPromoPiece(possibleMoves, myPosition, newPos);
+                    } else possibleMoves.add(new ChessMove(myPosition, newPos, null));
                 }
+            } else if (col == colToAdd && board.getPiece(newPos) == null) {
+                if (row == 2) {
+                    addPromoPiece(possibleMoves, myPosition, newPos);
+                } else possibleMoves.add(new ChessMove(myPosition, newPos, null));
             }
         }
+
 
         if (row == 7) {
             ChessPosition newPos1 = new ChessPosition(row - 1, col);
             ChessPosition newPos2 = new ChessPosition(row - 2, col);
             if (board.getPiece(newPos1) == null && board.getPiece(newPos2) == null) {
-                possibleMoves.add(new ChessMove(myPosition, newPos2, promo));
+                possibleMoves.add(new ChessMove(myPosition, newPos2, null));
             }
         }
 
         return possibleMoves;
+    }
+
+    private void addPromoPiece(Collection<ChessMove> set, ChessPosition myPosition, ChessPosition newPos) {
+        set.add(new ChessMove(myPosition, newPos, ChessPiece.PieceType.KNIGHT));
+        set.add(new ChessMove(myPosition, newPos, ChessPiece.PieceType.QUEEN));
+        set.add(new ChessMove(myPosition, newPos, ChessPiece.PieceType.BISHOP));
+        set.add(new ChessMove(myPosition, newPos, ChessPiece.PieceType.ROOK));
     }
 }
