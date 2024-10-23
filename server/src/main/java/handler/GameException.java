a9e35a86699a7afa850b42cc78f57198080a51c9
Tim Lencioni <tim.lencioni@gmail.com>
2024-10-23 10:39:18 -0600

    added error code parameter

--- server/src/main/java/handler/GameException.java
+++ server/src/main/java/handler/GameException.java
@@ -1,7 +1,12 @@
 package handler;
 
 public class GameException extends Exception {
-    public GameException(String message) {
+    private int errorNum;
+    public GameException(String message, int errorNum) {
+
         super(message);
+        this.errorNum = errorNum;
     }
+
+    public int getErrorNum() { return this.errorNum; }
 }
